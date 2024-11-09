e76c4e4c6dbb96b9100b3f22d8bb526c45f3125d
Thuan Nguyen <iamtnzzzz@gmail.com>
2024-11-09 13:39:37 +0700

    first commit

---
+++ src/main/java/app/techify/TechifyApplication.java
@@ -0,0 +1,13 @@
+package app.techify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/techify/TechifyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.techify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
